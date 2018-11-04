a161d180d79b808afdf7c6faaeafbb617ad1d26d
Rooniey - Marek Maras <210267@edu.p.lodz.pl>
2018-11-04 21:24:53 +0100

    Added async command for calling reflector and adjusted ui

---
+++ BusinessLogic/Base/IAsyncCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BusinessLogic.Base
+{
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync();
+
+        bool CanExecute();
+    }
+}
