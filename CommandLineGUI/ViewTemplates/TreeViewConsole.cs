37d74b57768a66c670bb9edf06df552f8a8c3c1f
Marek Maras <marek.maras2@gmail.com>
2018-11-09 01:29:24 +0100

    Console polishing up

--- CommandLineGUI/ViewTemplates/TreeViewConsole.cs
+++ CommandLineGUI/ViewTemplates/TreeViewConsole.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Runtime.InteropServices;
 using BusinessLogic.Model;
 using CommandLineGUI.Base;
 
